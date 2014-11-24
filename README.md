b96d53896987c92b476d4d20e6969ded36a227cf
Shivan <shivankaul.1993@gmail.com>
2014-11-24 13:55:07 -0500

    changed makefile and edited readme

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ FileSystem
 A simple filesystem implemented in C.  
 
 Use:  
-<pre><code>gcc -o MyThreads MyThreads.c -DHAVE_PTHREAD_RWLOCK=1 -lslack</code></pre>  
+<pre><code>gcc -o SFS sfs_api.c disk_emu.c -DHAVE_PTHREAD_RWLOCK=1 -lslack</code></pre>  
 for compiling.
--- src/sfs_api.c
+++ src/sfs_api.c
@@ -8,14 +8,14 @@
  */
 
 #include <stdio.h>
+#include "sfs_api.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <slack/std.h>
 #include <slack/map.h>
-#include <slack/list.h>
 
 //#define MAX_FILES 20 // this will be defined by the N (total blocks in CCDisk) - (# of blocks allocated for
-					// root directory and FAT) - 2 (the blocks for superblock and free block list)
+// root directory and FAT) - 2 (the blocks for superblock and free block list)
 
 /*
  * In memory data structures
@@ -24,8 +24,21 @@
 // Directory Table, implemented as a map between filenames and FAT indexes
 Map *directory_table;
 
-if (!(directory_table = map_create(NULL)))
-            return EXIT_FAILURE;
+int mksfs(int fresh) {
+	if (!fresh) { // re-open from disk
+		// the first file written to the disk should be
+		init_disk()
+	}
+	if (!(directory_table = map_create(NULL))) {
+		return EXIT_FAILURE;
+	}
+
+}
+
+int main() {
+	printf("Hello world!\n");
+}
+
 
 
 
